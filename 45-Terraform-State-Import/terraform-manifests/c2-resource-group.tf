aa37ce98ca07977894739627db9d8f2ea5af3875
ketan <ketandogra1367@gmail.com>
2024-10-17 01:24:23 +0530

    Terraform-State-Import

---
+++ 45-Terraform-State-Import/terraform-manifests/c2-resource-group.tf
@@ -0,0 +1,16 @@
+# terraform import azurerm_resource_group.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/example
+# terraform import azurerm_resource_group.example /subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RESOURCE_GROUP_NAME>
+# Resource Group
+# resource "azurerm_resource_group" "myrg" {
+#    name = "myrg1"
+#    location = "eastus"
+#    tags = {
+#      "Tag1" = "My-Tag-1"
+#    }
+# }
+
+
+
+
+
+
